4d9edec800bc1b4cc2ec760b1b203d31ee7a5d94
G-oracle <akinlolugbemisola@gmail.com>
2023-07-05 15:56:25 +0100

    WORK DONE: 	- Updated the README.md file 	- Created the providers.tf file 	- Changed the main.tf file from deploying simple AWS Resource to deploying a VPC consisting of the following: 		-- At least two public subnets 		-- At least two private subnets 		--Internet gateway 		-- At least two NAT gateways 		-- Public route table 		-- Private route table

--- main.tf
+++ main.tf
@@ -1,24 +1,151 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "us-east-2"
-}
-
-resource "aws_instance" "app_server" {
-    # I changed the ami id because the default is giving an error
-  ami           = "ami-03f38e546e3dc59e1"
-  instance_type = "t2.micro"
+# Create Main VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "main"
   }
+}
+
+# Create 2 public and 2 private subnets
+resource "aws_subnet" "public0" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public0"
+  }
+}
+
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "public1"
+  }
+}
+
+resource "aws_subnet" "private0" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.20.0/24"
+
+  tags = {
+    Name = "private0"
+  }
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.21.0/24"
+
+  tags = {
+    Name = "private1"
+  }
+}
+
+# Creates the Internet Gateway for the External Access to the VPC
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# Adding Elastic IP addresses for the NAT Gateways
+resource "aws_eip" "nat0" {
+  vpc = true
+
+  tags = {
+    Name = "nat0"
+  }
+}
+
+resource "aws_eip" "nat1" {
+  vpc = true
+
+  tags = {
+    Name = "nat1"
+  }
+}
+
+# Creates 2 NAT Gateways
+resource "aws_nat_gateway" "main0" {
+  allocation_id = aws_eip.nat0.id
+  subnet_id     = aws_subnet.public0.id
+
+  tags = {
+    Name = "main0"
+  }
+}
+
+resource "aws_nat_gateway" "main1" {
+  allocation_id = aws_eip.nat1.id
+  subnet_id     = aws_subnet.public1.id
+
+  tags = {
+    Name = "main1"
+  }
+}
+
+#Creates 1 Route Table for the Public Subnet and for each Private Subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table" "private0" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main0.id
+  }
+
+  tags = {
+    Name = "private0"
+  }
+}
+
+resource "aws_route_table" "private1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main1.id
+  }
+
+  tags = {
+    Name = "private1"
+  }
+}
+
+# Creates route paths for association between the subnets
+resource "aws_route_table_association" "public0" {
+  subnet_id      = aws_subnet.public0.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private0" {
+  subnet_id      = aws_subnet.private0.id
+  route_table_id = aws_route_table.private0.id
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private1.id
 }
