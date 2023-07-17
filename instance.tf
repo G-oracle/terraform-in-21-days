6c2bc0261657a4dc5cf6478afad27b9d83705b06
Akinlolu Gbemisola John <akinlolugbemisola@gmail.com>
2023-07-17 11:53:08 +0100

    TASK: Launch Your First EC2 Instance (#7)

---
+++ instance.tf
@@ -0,0 +1,53 @@
+data "aws_ami" "amazonlinux" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-kernel-*"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+  owners = ["137112412989"]
+}
+
+resource "aws_security_group" "public" {
+  name        = "${var.env_code}-public"
+  description = "Public SG"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_instance" "orac" {
+  ami                    = data.aws_ami.amazonlinux.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public[0].id
+  vpc_security_group_ids = [aws_security_group.public.id]
+  key_name               = "main"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
