a3449dfd5ca9bfca0ad900d8d26be5aa3d251441
G-oracle <akinlolugbemisola@gmail.com>
2023-07-17 17:20:51 +0100

    TASK: Implementing User Data and Deploy 2048 Game 	- Added an inbound rule to the Public SG 	- Installed Apache Webserver, git & Deployed 2048 Game  using user data 	- Added a trailing newline to the terraform.tfvars file

--- instance.tf
+++ instance.tf
@@ -2,15 +2,15 @@ data "aws_ami" "amazonlinux" {
   most_recent = true
 
   filter {
-    name = "name"
+    name   = "name"
     values = ["amzn2-ami-kernel-*"]
   }
 
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
-  
+
   owners = ["137112412989"]
 }
 
@@ -20,18 +20,26 @@ resource "aws_security_group" "public" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -40,12 +48,13 @@ resource "aws_security_group" "public" {
 }
 
 resource "aws_instance" "orac" {
-  ami                    = data.aws_ami.amazonlinux.id
-  instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.public[0].id
-  vpc_security_group_ids = [aws_security_group.public.id]
-  key_name               = "main"
+  ami                         = data.aws_ami.amazonlinux.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public[0].id
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  key_name                    = "main"
   associate_public_ip_address = true
+  user_data                   = file("user-data.sh")
 
   tags = {
     Name = "${var.env_code}-public"
