0888550008b7baafb42d23c007101943d688ba07
Akinlolu Gbemisola John <akinlolugbemisola@gmail.com>
2023-07-22 22:53:35 +0100

    TASK: IAM Roles (#14)
    * TASK: IAM Roles
    	- Created an IAM role & attached S3FullAccess policy to it.
    	- Attached this role to your Auto-Scaling group.
    	- Modified the Public_SG to allow SSH to the Public Instances
    
    * Terraform fmt

--- level2/instance.tf
+++ level2/instance.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "public" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
